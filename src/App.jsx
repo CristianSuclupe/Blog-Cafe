968601d1ed1648ef2c1c0184ab86cf0c7c9ee6b5
Cristian021000 <N00148478@upn.pe>
2023-08-20 15:29:28 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <header className="header">
+        <div>
+          <h1 className="no-margin">hola</h1>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default App;
